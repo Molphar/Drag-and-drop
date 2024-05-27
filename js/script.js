8ed82df3e77f12a716924cea191b60d8e2daf25d
HappyFucker <17487175+Molphar@users.noreply.github.com>
2024-05-28 01:26:58 +0300

    Fix code issues 1-5

--- js/script.js
+++ js/script.js
@@ -1,102 +1,128 @@
-window.onload = function(){
-  document.querySelector("#init").addEventListener("click", ReadElementsNumber);
-  let elements = document.querySelectorAll(".item");
+var numsInput;
+var containerItems;
+var initButt;
 
-  elements.forEach((element, index) => {
-    addDragListeners(element);
-    colorRect(element, index);
-  });
+window.onload = function(){
+    initButt = document.getElementById("init");
+    numsInput = document.getElementById("inpNumbers");
+    containerItems = document.querySelector(".container");
+
+    initButt.addEventListener("click", ReadElementsNumber);
+    let elements = document.querySelectorAll(".item");
+    
+    elements.forEach((el, index) => {
+        addDragListeners(el);
+        colorRect(el, index);
+    });
 }
 
 function ReadElementsNumber(){
-  let nums = document.querySelector("#inpNumbers").value;
-  let rectCount =  document.querySelectorAll(".item").length;
-
-  let element = document.createElement("div");
-  element.appendChild(document.createElement("span"));
-  element.classList.add("item");
-  let elements = [];
-  let outerElement = document.querySelector(".container");
-  
-  for(let i = 0; i < nums; i++)
-  elements.push(element.cloneNode(true));
-  
-  elements.forEach((el, index) => {
-    addDragListeners(el);
-    colorRect(el, index + rectCount);
-    el.querySelector("span").innerText = index + rectCount + 1;
-    outerElement.appendChild(el);
-  })
+    const nums = numsInput.value;
+    const rectCount = document.querySelectorAll(".item").length;
+    let element = document.createElement("div");
+    element.appendChild(document.createElement("span"));
+    element.classList.add("item");
+    let elements = [];
+    
+    for(let i = 0; i < nums; i++){
+        elements.push(element.cloneNode(true));
+    }
+    
+    elements.forEach((el, index) => {
+        addDragListeners(el);
+        colorRect(el, index + rectCount);
+        el.querySelector("span").innerText = index + rectCount + 1;
+        containerItems.appendChild(el);
+    })
 }
 
 function colorRect(el, index){
-  el.style.background = `rgb(${5*index}, ${5*index}, ${255 - index})`;
+    el.style.background = `rgb(${5*index}, ${5*index}, ${255 - index})`;
 }
 
 function addDragListeners(el){
-  el.addEventListener("touchstart", dragStart);
-  el.addEventListener("touchmove", drag, false);
-  el.addEventListener("touchend", dragEnd, false);
-  el.addEventListener("mousedown", dragStart);
-  el.addEventListener("mousemove", drag, false);
-  el.addEventListener("mouseup", dragEnd, false);
+    el.addEventListener("touchstart", onTouchDragStart);
+    el.addEventListener("touchmove", onTouchDrag, false);
+    el.addEventListener("touchend", onDragEnd, false);
+    el.addEventListener("mousedown", onMouseDragStart);
+    el.addEventListener("mousemove", onMouseDrag, false);
+    el.addEventListener("mouseup", onDragEnd, false);
 }
 
-var active = false;
+var isDragging = false;
 var currentX;
 var currentY;
 var initialX;
 var initialY;
-var oofsetX = 0;
-var oofsetY = 0;
+var offsetX = 0;
+var offsetY = 0;
 
-function dragStart(e) {
- 
-  if (e.type === "touchstart") {
-    initialX = e.touches[0].clientX;
-    initialY = e.touches[0].clientY;
-  } else {
-    initialX = e.clientX;
-    initialY = e.clientY;
-  }
-
-  let transformProp = e.target.style.transform;
-  if(transformProp !== ""){
-    let transofm = transformProp.slice(transformProp.indexOf("(") + 1, transformProp.length - 1  );
-    [oofsetX, oofsetY] = transofm.split("px, ", 2);
-  }  
-    active = true;
+function onMouseDragStart(mouseEvent) {
+    let x = mouseEvent.clientX;
+    let y = mouseEvent.clientY;
+    dragStart(x, y, mouseEvent.target);
 }
-    
-// console.log(Hello);
 
-function drag(e) {
-  if (active) {
-    
-    e.preventDefault();
-    
-    if (e.type === "touchmove") {
-      currentX = e.touches[0].clientX - initialX;
-      currentY = e.touches[0].clientY - initialY;
-    } else {
-      currentX = e.clientX - initialX;
-      currentY = e.clientY - initialY;
+function onTouchDragStart(touchEvent) {
+    let touch = touchEvent.touches[0];
+    let x = touch.clientX;
+    let y = touch.clientY;
+    dragStart(x, y);
+}
+
+function dragStart(x, y, el){
+    initialX = x;
+    initialY = y;
+
+    let transformProp = el.style.transform;
+    if(transformProp !== ""){
+        let transofm = transformProp.slice(transformProp.indexOf("(") + 1, transformProp.length - 1    );
+        [offsetX, offsetY] = transofm.split("px, ", 2);
+    }else{
+        offsetX = 0;
+        offsetY = 0;
     }
+    isDragging = true;
 
-    currentX += +oofsetX;
-    currentY += +oofsetY;
-
-    setTranslate(currentX, currentY, e.target);
-  }
+    console.log("dragStart: ", true);
+    console.log("initialX", initialX);
+    console.log("initialY", initialY);
 }
 
-function dragEnd(e) {
-  initialX = currentX;
-  initialY = currentY;
+function onTouchDrag(touchEvent) {
+    if (isDragging) {
+        touchEvent.preventDefault();
+        let touch = touchEvent.touches[0]; 
+        let x = touch.clientX;
+        let y = touch.clientY;
+        drag(x, y, touchEvent.target);
+    }
+}
 
-  active = false;
+function onMouseDrag(mouseEvent) {
+    if (isDragging) {
+        mouseEvent.preventDefault();
+        let x = mouseEvent.clientX;
+        let y = mouseEvent.clientY;
+        drag(x, y, mouseEvent.target);
+    }
+}
+
+function drag(x, y, el){
+    currentX = x - initialX + Number(offsetX);
+    currentY = y - initialY + Number(offsetY);
+    setTranslate(currentX, currentY, el);
+    console.log("drag: ", true);
+    console.log("currentX ", currentX);
+    console.log("currentY ", currentY);
+}
+
+function onDragEnd(e) {
+    initialX = currentX;
+    initialY = currentY;
+    isDragging = false;
 }
 
 function setTranslate(xPos, yPos, el) {
-  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
 }
